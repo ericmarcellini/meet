c9589783c8f1f3d3af31c30216480c90c903882b
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-09-28 20:32:03 +0200

    exercise 4.2

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -90,10 +90,56 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": '*',
     },
     body: JSON.stringify({
       authUrl: authUrl,
     }),
   };
 };
+
+///
+module.exports.getCalendarEvents = async (event) => {
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+    oAuth2Client.setCredentials({ access_token });
+  
+    return new Promise((resolve, reject) => {
+
+      oAuth2Client.getToken(access_token,(error, response)=> {
+        calendar.events.list(
+          {
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          }
+        );
+      })  ;
+    })
+      .then((results)=>{
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ events: results.data.items })
+      };  
+      })
+      .catch((err)=>{
+        console.error(err);
+        return{
+          statusCode: 500,
+          body: JSON.stringify(err),
+        };
+      });
+  }
