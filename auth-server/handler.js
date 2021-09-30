8f029643fcd77a006dc700114cb8d11211a7c29d
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-09-30 22:41:38 +0200

    test

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -55,6 +55,9 @@ module.exports.getAccessToken = async (event) => {
     // Respond with OAuth token
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": '*',
+      },
       body: JSON.stringify(token),
     };  
     })
