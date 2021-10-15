1f95ce34411420f6aa35cdd4381ad29ea07e9bb9
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-10-15 17:19:04 +0200

    renamed variable location to defaultLocation as it was causing trouble

--- src/App.js
+++ src/App.js
@@ -1,76 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css'; 
 
+
 class App extends Component {
+
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
-    currentLocation: 'all'
+    defaultLocation: 'all'
   }
-
-
+ 
   componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ 
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events) 
-          });
+   this.mounted = true;
+   getEvents().then((events) => {
+     if (this.mounted) {
+     this.setState({ 
+       events: events.slice(0, this.state.numberOfEvents),
+       locations: extractLocations(events) 
+       });
       }
     })
     .catch(error => {
-      console.log(error);
-    })
-  }
-
-  componentWillUnmount(){
-    this.mounted = false;
-  }
-
+     console.log(error);
+   })
+ }
+ 
+ componentWillUnmount() {
+   this.mounted = false;
+ }
+ 
   updateEvents = (location, eventCount) => {
-    console.log(eventCount)
     getEvents().then((events) => {
-      let locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-        locationEvents = locationEvents.slice(0, eventCount)
+      let locationEvents = (location === 'all') ? events : 
+     events.filter((event) => event.location === location);
+     locationEvents = locationEvents.slice(0, eventCount)
       this.setState({
         events: locationEvents,
-        currentLocation: location
+        defaultLocation: location
       });
       console.log(events)
       console.log(locationEvents)
       console.log(location)
     });
   }
-
+ 
   updateEventNumber = (eventCount) => {
     this.setState({
-      numberOfEvents: eventCount,
+      numberOfEvents: eventCount
     });
-    const { currentLocation } = this.state;
-    this.updateEvents(currentLocation, eventCount)
-  } 
- //<NumberOfEvents updateEventNumber={(e)=> this.updateEventNumber(e)} numberOfEvents={numberOfEvents}/>
-  render() {
-     const { numberOfEvents, locations, events } = this.state
-    return (
-      <div className="App">
-        
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <NumberOfEvents updateEventCount={(e) => this.updateEventCount(e)} numberOfEvents={numberOfEvents} />
-        <EventList events={this.state.events}/>
-        
-      </div>
-    );
-  }
-}
-
-export default App;
+    const { defaultLocation } = this.state;
+    this.updateEvents(defaultLocation, eventCount);
+  };
+ 
+   render() {
+     const { numberOfEvents } = this.state;
+     return (
+       <div className="App">
+         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+         <NumberOfEvents updateEventNumber={(e) => this.updateEventNumber(e)} numberOfEvents={numberOfEvents} />
+         <EventList events={this.state.events}/>
+       </div>
+     );
+   }
+ }
+ 
+ export default App;
