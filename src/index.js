8cc3bececa36c564cf739fc32726fe80b8e22737
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-10-27 23:44:22 +0200

    serviceworkerreg called

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './service-worker';
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 
 
@@ -18,8 +19,8 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
-
+//serviceWorker.register();
+serviceWorkerRegistration.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
