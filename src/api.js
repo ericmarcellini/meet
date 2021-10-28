017a78c8738361b0925de323a80d2e1bfa0c6dca
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-10-28 16:44:06 +0200

    Offline capabilities

--- src/api.js
+++ src/api.js
@@ -59,6 +59,12 @@ import NProgress from "nprogress";
      return mockData;
    };
 
+   if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(events).events:[];;
+  };
+
    const token = await getAccessToken();
    if (token){
      removeQuery();
